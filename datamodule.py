70d80a16f56855bf126f68f76d266ae842b2cd2d
Suraj K <48147068+CaptainO5@users.noreply.github.com>
2024-04-20 16:22:23 -0400

    Update datamodule.py

--- datamodule.py
+++ datamodule.py
@@ -1,51 +1,45 @@
+import numpy as np
+import pandas as pd
 import pytorch_lightning as pl
-from torch.utils.data import Dataset, DataLoader
-from sklearn.model_selection import train_test_split
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM
+from torch.optim import AdamW
+import evaluate
 
-class SarcasmDataset(Dataset):
-    def __init__(self, data, tokenizer):
-        self.inputs = list(data.inputs) 
-        self.targets = list(data.targets)
-        self.tokenizer = tokenizer
-
-    def __len__(self):
-        return len(self.inputs)
-
-    def __getitem__(self, index):
-        input_text = self.inputs[index]
-        target_text = self.targets[index]
-        return self.tokenizer(input_text, text_target=target_text, max_length=128, truncation=True) #TODO make max_length part of config dict
-    
-class SarcasmDataModule(pl.LightningDataModule):
-    def __init__(self, data, tokenizer, collator):
-        super().__init__()
+from datamodule import SarcasmDataModule
+ 
+class Model(pl.LightningModule):
+    def __init__(self, model_name, lr):
+        super(Model, self).__init__()
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.lr = lr        
         
-        self.collator = collator
-        
-        self.train_df, val_df = train_test_split(data, shuffle=False, test_size=0.4) #TODO define test_size in config dict
-        self.val_df, self.test_df = train_test_split(val_df, shuffle=False, test_size=0.5)
-        self.train_dataset = SarcasmDataset(self.train_df, tokenizer)
-        self.test_dataset = SarcasmDataset(self.test_df, tokenizer)
-        self.val_dataset = SarcasmDataset(self.val_df, tokenizer)
+    def forward(self, batch):
+        output = self.model(**batch)
+        return output
+    
+    def generate(self, input_ids, attention_mask, max_length=128):
+        return self.model.generate(input_ids, attention_mask=attention_mask, max_length=max_length)
+    
+    def _step(self, batch, idx):
+        output = self(batch)
+        loss = output.loss
+        return loss
+    
+    def training_step(self, batch, idx):
+        loss = self._step(batch, idx)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+    
+    def validation_step(self, batch, idx):
+        loss = self._step(batch, idx)
+        self.log('val_loss', loss, prog_bar=True)
+        return loss
+    
+    def test_step(self, batch, idx):
+        loss = self._step(batch, idx)
+        self.log('test_loss', loss, prog_bar=True)
+        return loss
 
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            collate_fn=self.collator,
-            batch_size=32, #TODO define config and store the values outside of here
-            shuffle=True,
-        )
-    
-    def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            collate_fn=self.collator,
-            batch_size=16,
-        )
-    
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_dataset,
-            collate_fn=self.collator,
-            batch_size=4,
-        )
+    def configure_optimizers(self):
+        return AdamW(self.parameters(), lr=self.lr)
